3726394b0d96274faf5dfbeef3529878a2d0095a
husseinbouik <husseinbouik5@gmail.com>
2023-11-27 09:43:31 +0100

    search ajax

--- Branche technique/Labs/lab-laravel-basic/resources/views/base.blade.php
+++ Branche technique/Labs/lab-laravel-basic/resources/views/base.blade.php
@@ -174,6 +174,16 @@
 
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="{{ asset('js/pages/dashboard.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/browser-resolve/1.14.2/browser.resolve.js"></script>
+<!-- MDB -->
+<script
+type="text/javascript"
+src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.2/mdb.min.js"
+></script>
 
 </body>
 
